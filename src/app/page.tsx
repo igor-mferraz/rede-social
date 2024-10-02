ac86114ba7d18c09063a4c6e11f9ee17bf046451
igor-mferraz <igormanoelferraz2022@gmail.com>
2024-10-01 22:33:01 -0300

    layout created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
+import { Logo } from "@/components/ui/logo";
+import { redirect } from "next/navigation";
 
 export default function Page() {
+
+  redirect('/home')
+
   return (
-    <div>
-      ...
+    <div className="min-h-screen justify-center items-center">
+      <Logo
+        size={80}
+      />
     </div>
   );
 }
